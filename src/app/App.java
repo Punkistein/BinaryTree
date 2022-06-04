86b46567b5245a729181871bb49c5cf880344ea9
User <User@Ryzen5155-Gide>
2022-06-04 15:42:14 -0500

    - Ya se crean las filas dinámicas - Se imprimen el número de preguntas

--- src/app/App.java
+++ src/app/App.java
@@ -3,7 +3,7 @@ package app;
 import vista.VistaPrincipal;
 
 public class App {
-    
+    //este comentario es de saul
     static VistaPrincipal objVP = new VistaPrincipal();
     
     public static void main(String[] args) {
